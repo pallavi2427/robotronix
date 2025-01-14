558414823aa1c97d9010de7a4fe1bceac4417b14
shabdasinha1 <shabdasinha11@gmail.com>
2025-01-14 17:10:45 +0530

    converted the meandev page into separate components

--- src/component/screen/MeanDev.jsx
+++ src/component/screen/MeanDev.jsx
@@ -1,149 +1,44 @@
-import meanLogo from "./../../assets/img/MEAN-Stack.jpg";
-import section5Image from "./../../assets/img/mean-section5-img.jpg";
+import SectionFront from "./SectionFront";
+import { Data } from "./Data";
+import InfoSection from "./InfoSection";
+import ExpertTalkSection from "./ExpertTalkSection";
+import TechStackSection from "./TechStackSection";
+import HiringSection from "./HiringSection";
 
 const MeanDev = () => {
-  const cardData = [
-    {
-      heading: "MongoDB for storage",
-      content:
-        "MongoDB is an open-source NoSQL database that allows enterprises to be more agile and expandable. MongoDB has a flexible structure and can support a variety of formats of data.",
-    },
-    {
-      heading: "ExpressJs for web programming",
-      content:
-        "Express.JS is a lightweight but adaptable Node.JS web-based application framework that gives us a wide range of capabilities to create single-page web applications and multipage web and mobile apps. Node.JS facilitates routing.",
-    },
-    {
-      heading: "AngularJS for front-end development",
-      content:
-        "AngularJS is an open-source web application framework designed for dynamic web applications, which allows you to expand the capabilities of your HTML language application. This JavaScript-developed development framework is clear and easy to read and build.",
-    },
-    {
-      heading: "NodeJS for Web Server",
-      content:
-        "Node JS is an open-source, cross-platform used to create efficient and scalable server-side apps. Node.JS apps are developed with Google’s V8 JavaScript Engine, with an extensive JavaScript modules library.",
-    },
-  ];
   return (
     <>
       <div className="container-fluid">
         {/* first section start */}
-        <div className="row mean-section1">
-          <div className="col-6 section-divide">
-            <div className="content">
-              <h1>MEAN Stack Development Company</h1>
-              <p>
-                One stack that harnesses the powerful capabilities of four
-                different platforms. Use the MVP design of MEAN Stack to build
-                powerful web-based experiences.
-              </p>
-              <br />
-              <br />
-
-              <button type="button" class="btn btn-lg">
-                Get in Touch
-              </button>
-            </div>
-          </div>
-          <div className="col-6 section-divide">
-            <div className="mean-img">
-              <img src={meanLogo} alt="Mean Logo" />
-            </div>
-          </div>
-        </div>
+        <SectionFront
+          title={Data.meanDev.sectionFront.title}
+          content={Data.meanDev.sectionFront.content}
+          imageSrc={Data.meanDev.sectionFront.img}
+        />
         {/* first section end */}
         {/* second section start */}
-        <div className="row mean-section2">
-          <h1>The combination of fantastic four</h1>
-          <p>
-            The MEAN Stack, comprising MongoDB, Express.js, Angular, and
-            Node.js, is a powerful combination of technologies that work
-            seamlessly together to build dynamic, high-performance web
-            applications. Each component plays a crucial role in delivering a
-            full-stack development experience: MongoDB offers a scalable, NoSQL
-            database; Express.js simplifies server-side development; Angular
-            provides a robust front-end framework; and Node.js powers the
-            backend with its event-driven, non-blocking I/O model. Together,
-            these four technologies form a robust, efficient, and scalable
-            solution for modern web development, making the MEAN Stack an ideal
-            choice for developers seeking to create innovative, end-to-end
-            applications.
-          </p>
-        </div>
+        <InfoSection
+          title={Data.meanDev.infoSection.title}
+          content={Data.meanDev.infoSection.content}
+        />
         {/* second section end */}
         {/* third section start */}
-        <div className="row mean-section3">
-          <h1>Talk to expert</h1>
-          <p>
-            Robotronix has established itself as a leading name in MEAN Stack
-            development, delivering exceptional products to clients worldwide.
-            Since its inception, Robotronix has specialized in offering tailored
-            MEAN Stack solutions, producing innovative and industry-defining
-            products that reflect our creative approach and forward-thinking
-            development strategies.
-          </p>
-          <button type="button" class="btn btn-lg">
-            Reach out to learn more
-          </button>
-          <hr />
-        </div>
-
+        <ExpertTalkSection content={Data.meanDev.expertTalkSection.content} />
         {/* third section end */}
         {/* fourth section start */}
-        <div className="row mean-section4">
-          <div className="tech-stack-section">
-            <h1>Tech stack elements in MEAN</h1>
-            <p>
-              The MEAN Stack is a popular and versatile full-stack JavaScript
-              framework designed for building dynamic web applications. MEAN
-              stands for MongoDB, Express JS, Node JS.
-            </p>
-          </div>
-          <div className="cards-section">
-            {cardData.map((item) => {
-              return (
-                <>
-                  <div className="flip-card">
-                    <div className="flip-card-inner">
-                      <div className="flip-card-front">
-                        <h2>{item.heading}</h2>
-                      </div>
-                      <div className="flip-card-back">
-                        <p>{item.content}</p>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </div>
+        <TechStackSection
+          title={Data.meanDev.techStackSection.title}
+          content={Data.meanDev.techStackSection.content}
+          cardData={Data.meanDev.techStackSection.cardData}
+        />
+        <hr />
         {/* fourth section end */}
         {/* fifth section start */}
-        <div className="row mean-section5">
-          <div className="col-6 hiring-image">
-            <img src={section5Image} alt="Hiring" />
-          </div>
-          <div className="col-6 content">
-            <h1>
-              Bring on board expert <br /> MEAN stack developers
-            </h1>
-            <br />
-            <p>
-              At Robotronix, our MEAN Stack specialists leverage the full
-              potential of JavaScript technologies, utilizing cutting-edge tools
-              and staying ahead of the latest market trends to deliver
-              exceptional solutions. Our expert MEAN Stack team is equipped to
-              provide tailored solutions for any industry, whether it's
-              entertainment, eCommerce, banking, healthcare, or beyond, ensuring
-              that each project aligns perfectly with our clients' unique needs
-              and objectives.
-            </p>
-            <button type="button" class="btn btn-lg">
-                Get in Touch
-              </button>
-          </div>
-        </div>
+        <HiringSection
+          title={Data.meanDev.hiringSection.title}
+          content={Data.meanDev.hiringSection.content}
+          imageSrc={Data.meanDev.hiringSection.img}
+        />
         {/* fifth section end */}
       </div>
     </>
