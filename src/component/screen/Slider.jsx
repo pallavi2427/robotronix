3d78892067601fd3281e8775c4cd7d49c037ebc3
pallavi2427 <pallavi.sisodiya9009@gmail.com>
2025-01-15 16:42:04 +0530

    home page section

--- src/component/screen/Slider.jsx
+++ src/component/screen/Slider.jsx
@@ -8,7 +8,16 @@ import logoSlider4 from "./../../assets/img/honda_logo1.png";
 import logoSlider5 from "./../../assets/img/pinnacle1.png";
 import logoSlider6 from "./../../assets/img/airport_authority_of_india1.png";
 import logoSlider7 from "./../../assets/img/mpidc_logo1.png";
-import wwd from "./../../assets/img/earth-the-world-earth-carbon-wallpaper-thumb.jpg"
+import wwd from "./../../assets/img/earth-the-world-earth-carbon-wallpaper-thumb.jpg";
+import technical from "./../../assets/img/tech.jpg";
+import consult from "./../../assets/img/consult.jpg";
+import service from "./../../assets/img/service.jpg";
+import cyber from "./../../assets/img/cyber-security.jpg";
+import dataAnalysis from "./../../assets/img/analytics.jpg";
+import ai from "./../../assets/img/ai.jpg";
+import wdevelopment from "./../../assets/img/web.jpg";
+import digital from "./../../assets/img/dm.jpg";
+import { NavLink } from "react-router-dom";
 
 const Slider = () => {
   const [activeTab, setActiveTab] = useState(0);
@@ -16,9 +25,182 @@ const Slider = () => {
   const handleTabClick = (index) => {
     setActiveTab(index);
   };
+  const tabs = [
+    {
+      title: "Technical Support",
+      content: (
+        <>
+          <div className='row'>
+            <div className='col-lg-6 mt-2'>
+              <img
+                src={technical}
+                alt='Technical Support'
+                style={{ width: "100%", height: "180px" }}
+              />
+            </div>
+            <div className='col-lg-6'>
+              <p style={{ fontWeight: "600" }}>
+                <ul>
+                  <li>
+                    Assistance with troubleshooting software or hardware
+                    issues..
+                  </li>
+                  <li> Help with network connectivity problems. </li>
+                  <li>
+                    Guidance on resolving account login or password reset
+                    issues.
+                  </li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </>
+      ),
+    },
+    {
+      title: "Consultation Services",
+      content: (
+        <>
+          <div className='row'>
+            <div className='col-lg-6 mt-2'>
+              <img
+                src={consult}
+                alt='Consultation Services'
+                style={{ width: "100%", height: "180px" }}
+              />
+            </div>
+            <div className='col-lg-6'>
+              <p></p>
+              <p style={{ fontWeight: "600" }}>
+                <ul>
+                  <li>Advice on IT infrastructure upgrades.</li>
+                  <li>
+                    Support in choosing the right software or tools for your
+                    business.{" "}
+                  </li>
+                  <li>IT strategy planning for business growth.</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </>
+      ),
+    },
+    {
+      title: "Product Information",
+      content: (
+        <>
+          <div className='row'>
+            <div className='col-lg-6 mt-2'>
+              <img
+                src={service}
+                alt='Product or Service Information'
+                style={{ width: "100%", height: "180px" }}
+              />
+            </div>
+            <div className='col-lg-6'>
+              <p style={{ fontWeight: "600" }}>
+                <ul>
+                  <li>
+                    {" "}
+                    Detailed explanations of IT solutions offered (e.g., cloud
+                    services, cybersecurity, managed IT).
+                  </li>
+                  <li>Demonstrations or free trials of software or tools.</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </>
+      ),
+    },
+    {
+      title: "Cybersecurity",
+      content: (
+        <>
+          <div className='row'>
+            <div className='col-lg-6 mt-2'>
+              <img
+                src={cyber}
+                alt='Cybersecurity'
+                style={{ width: "100%", height: "180px" }}
+              />
+            </div>
+            <div className='col-lg-6'>
+              <p style={{ fontWeight: "600" }}>
+                <ul>
+                  <li>Guidance on securing your systems and data.</li>
+                  <li> Support in handling data breaches or threats.</li>
+
+                  <li> Training on cybersecurity best practices.</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </>
+      ),
+    },
+    {
+      title: "Data and Analytics",
+      content: (
+        <>
+          <div className='row'>
+            <div className='col-lg-6 mt-2'>
+              <img
+                src={dataAnalysis}
+                alt='Data and Analytics'
+                style={{ width: "100%", height: "180px" }}
+              />
+            </div>
+            <div className='col-lg-6'>
+              <p style={{ fontWeight: "600" }}>
+                <ul>
+                  <li>Assistance in setting up data analytics platforms.</li>
+                  <li>Help with data backup and recovery services. </li>
+                  <li>
+                    Custom reports or insights to support decision-making.
+                  </li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </>
+      ),
+    },
+  ];
+  const contentData = [
+    {
+      title: "AI/ML",
+      description:
+        "Explore the power of AI/ML to drive automation, innovation, and insights with cutting-edge technology.",
+      image: ai, // Assuming `ai` is an imported image
+      link: "/mean-development",
+      borderColor: "#DAFF3E",
+      hoverTextColor: "#242424",
+    },
+    {
+      title: "WEB DEVELOPMENT",
+      description:
+        "Build robust web solutions tailored to your needs using the latest technologies and best practices.",
+      image: wdevelopment, // Assuming `wdevelopment` is an imported image
+      link: "/web-development",
+      borderColor: "#9F4EFF",
+      hoverTextColor: "#242424",
+    },
+    {
+      title: "DIGITAL MARKETING",
+      description:
+        "Enhance your online presence and reach your audience with effective digital marketing strategies.",
+      image: digital, // Assuming `digital` is an imported image
+      link: "/digital-marketing",
+      borderColor: "#9F4EFF",
+      hoverTextColor: "#242424",
+    },
+  ];
+
   return (
     <>
-      {/* Hero Section */}
+      {/* Slider Section Start */}
       <main className='main'>
         <div
           id='carouselExampleInterval'
@@ -55,94 +237,94 @@ const Slider = () => {
             <span className='visually-hidden'>Next</span>
           </button>
         </div>
-        {/* /Hero Section */}
+        {/* /Slider Section End */}
+        {/* ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+        {/* card start */}
 
-        {/* About Section */}
+        <div className='container'>
+          <div className='d-flex justify-content-center align-item-center'>
+            <h2 className='opacity-50 content-subtitle'>What We Do</h2>
+          </div>
+          <div className='d-flex justify-content-center align-item-center'>
+            <p className='content-para'>
+              We create innovative solutions to transform your workplace.
+            </p>
+          </div>
+          <div className='row'>
+            {contentData.map((item, index) => (
+              <div className='col-lg-4' key={index}>
+                <NavLink to={item.link}>
+                  <div
+                    className='file-container'
+                    data-border-color={item.borderColor}
+                    data-hover-text-color={item.hoverTextColor}>
+                    <div className='inner-container'>
+                      <div className='image-container'>
+                        <img
+                          src={item.image}
+                          alt={item.title}
+                          className='file-image'
+                        />
+                      </div>
+                      <div className='content'>
+                        <h1 className='text'>
+                          <span className='title'>{item.title}</span>
+                          <span className='text-effect' />
+                        </h1>
+                        <p className='description'>{item.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </NavLink>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* card End */}
+        {/* ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+        {/* How we can help you Section start */}
         <div className='d-flex justify-content-center align-item-center mt-5'>
           {" "}
-          <h2 className='opacity-50 content-subtitle'>WHAT WE DO</h2>
+          <h2 className='opacity-50 content-subtitle'>How we can help you?</h2>
         </div>
         <div className='d-flex justify-content-center align-item-center'>
           {" "}
-          <p className='content-para'>In creating truly digital workplace</p>
+          <p className='content-para'>
+            We provide innovative solutions to help you build a truly digital
+            workplace.
+          </p>
         </div>
         <div className='about section'>
           <div id='about' className='about'>
             <div className='content'>
               <div className='container-fluid'>
                 <div className='row'>
-                  <div className='col-lg-8 mb-4 mb-lg-0'>
-                    {/* navs tabs start */}
+                  <div className='col-lg-12 mb-4 mb-lg-0'>
                     <div className='cycle-tab-container'>
-                      <ul className='nav nav-tabs  justify-content-center flex-column flex-sm-row'>
-                        {["Transform", "Develop", "Improve"].map(
-                          (tab, index) => (
-                            <li
-                              key={index}
-                              className={`cycle-tab-item ${
-                                activeTab === index ? "active" : ""
-                              }`}
-                              onClick={() => handleTabClick(index)}>
-                              <a className='nav-link border-none'>{tab}</a>
-                            </li>
-                          )
-                        )}
+                      <ul
+                        className='nav nav-tabs justify-content-center flex-column flex-sm-row'
+                        role='tablist'>
+                        {tabs.map((tab, index) => (
+                          <li
+                            key={index}
+                            className={`cycle-tab-item ${
+                              activeTab === index ? "active" : ""
+                            }`}
+                            onClick={() => handleTabClick(index)}
+                            role='tab'
+                            aria-selected={activeTab === index}>
+                            <a
+                              href='#!'
+                              className='nav-link border-none'
+                              onClick={(e) => e.preventDefault()}>
+                              {tab.title}
+                            </a>
+                          </li>
+                        ))}
                       </ul>
                       <div className='tab-content'>
-                        {[
-                          {
-                            title: "Transform",
-                            content: (
-                              <>
-                                <p>
-                                  Welcome to the home tab. Here you'll find
-                                  general information about our services and
-                                  updates.
-                                </p>
-                                <p>
-                                  Lorem ipsum dolor sit amet, consectetur
-                                  adipiscing elit. Praesent suscipit sem at
-                                  lectus hendrerit, ac tincidunt urna malesuada.
-                                  Sed imperdiet orci eget nunc faucibus, vitae
-                                  euismod est lobortis.
-                                </p>
-                              </>
-                            ),
-                          },
-                          {
-                            title: "Develop",
-                            content: (
-                              <>
-                                <p>
-                                  In your profile tab, you can manage your
-                                  personal details and update your preferences.
-                                </p>
-                                <p>
-                                  Nulla facilisi. Curabitur non libero sit amet
-                                  eros malesuada ullamcorper. Nunc scelerisque
-                                  metus in urna sollicitudin feugiat.
-                                </p>
-                              </>
-                            ),
-                          },
-                          {
-                            title: "Improve",
-                            content: (
-                              <>
-                                <p>
-                                  Here in the messages tab, you can read and
-                                  send messages to stay connected with others.
-                                </p>
-                                <p>
-                                  Fusce vestibulum risus at mi hendrerit, sed
-                                  gravida velit tincidunt. Sed tempus vehicula
-                                  odio eu condimentum. Integer vel turpis
-                                  sapien.
-                                </p>
-                              </>
-                            ),
-                          },
-                        ].map((tabContent, index) => (
+                        {tabs.map((tabContent, index) => (
                           <div
                             key={index}
                             className={`tab-pane fade ${
@@ -156,79 +338,26 @@ const Slider = () => {
                         ))}
                       </div>
                     </div>
-
-                    {/* nav tabs end */}
                   </div>
-                  <div
-                    className='col-lg-4 mb-lg-0 ml-auto'
-                    data-aos='fade-up'
-                    data-aos-delay={100}>
-                    <h2 className='content-title mb-4'>
-                      <strong>Customized Solutions</strong> and{" "}
-                      <strong>Effortless </strong>
-                      Communication Channels
-                    </h2>
-                    <div className='section-2'>
-                      <img src={wwd}></img>{" "}
-                      <p className='opacity-50'>
-                        <strong style={{ color: "#08bdee" }}
-                        >
-                          Robotronix India
-                        </strong>{" "}
-                        interacts with clients to identify their requirements
-                        and accordingly designs, develops, and implements an
-                        integrated and comprehensivev solution. We are
-                        accessible in the way you prefer: phone, email, text,
-                        instant messenger, and even in-person meetings.
-                      </p>
-                    </div>
-                    {/* <p className='opacity-50'>
-                      <strong style={{ color: "#08bdee" }}>
-                        Robotronix India
-                      </strong>{" "}
-                      interacts with clients to identify their requirements and
-                      accordingly designs, develops, and implements an
-                      integrated and comprehensivev solution. We are accessible
-                      in the way you prefer: phone, email, text, instant
-                      messenger, and even in-person meetings.
-                    </p> */}
-
-                    {/* <div className='row my-5'>
-                      <div className='col-lg-12 d-flex align-items-start mb-4'>
-                        <i className='bi bi-cloud-rain me-4 display-6' />
-                        <div>
-                          <h4 className='m-0 h5'>Plants needs rain</h4>
-                          <p className='text-white opacity-50'>
-                            Lorem ipsum dolor sit amet.
-                          </p>
-                        </div>
-                      </div>
-                      <div className='col-lg-12 d-flex align-items-start mb-4'>
-                        <i className='bi bi-heart me-4 display-6' />
-                        <div>
-                          <h4 className='m-0 h5'>Love organic foods</h4>
-                          <p className='text-white opacity-50'>
-                            Lorem ipsum dolor sit amet.
-                          </p>
-                        </div>
-                      </div>
-                      <div className='col-lg-12 d-flex align-items-start'>
-                        <i className='bi bi-shop me-4 display-6' />
-                        <div>
-                          <h4 className='m-0 h5'>Sell vegies</h4>
-                          <p className='text-white opacity-50'>
-                            Lorem ipsum dolor sit amet.
-                          </p>
-                        </div>
-                      </div>
-                    </div> */}
-                  </div>
+                  {/* Additional content can go here */}
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* Logo section */}
+        {/*How we can help you Section end */}
+
+        {/* ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+
+        {/* Logo section start*/}
+        <div className='d-flex justify-content-center align-item-center mt-5'>
+          {" "}
+          <h2 className='opacity-50 content-subtitle'>Our Clients</h2>
+        </div>
+        <div className='d-flex justify-content-center align-item-center'>
+          {" "}
+          <p className='content-para'>Trusted by clients nationwide.</p>
+        </div>
         <section className='logo-section'>
           <div
             className='container-fluid section-title'
@@ -355,8 +484,6 @@ const Slider = () => {
           </div>
         </section>
         {/* Logo section end */}
-
-        {/* /About Section */}
       </main>
     </>
   );
