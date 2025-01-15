937bd763ab6a4ccdee0abbec3829056e357d955c
shabdasinha1 <shabdasinha11@gmail.com>
2025-01-15 17:27:56 +0530

    Added content to reactDev page

--- src/component/screen/Data.jsx
+++ src/component/screen/Data.jsx
@@ -1,53 +1,115 @@
 import meanLogo from "./../../assets/img/MEAN-Stack.jpg";
 import meanHiringImage from "./../../assets/img/mean-section5-img.jpg";
 
-  
-  export const Data = {
-    meanDev:{
-        sectionFront:{
-            title:"MEAN Stack Development Company",
-            content:"One stack that harnesses the powerful capabilities of four different platforms. Use the MVP design of MEAN Stack to build powerful web-based experiences.",
-            img:meanLogo
+export const Data = {
+  meanDev: {
+    sectionFront: {
+      title: "MEAN Stack Development Company",
+      content:
+        "One stack that harnesses the powerful capabilities of four different platforms. Use the MVP design of MEAN Stack to build powerful web-based experiences.",
+      img: meanLogo,
+    },
+    infoSection: {
+      title: "The Combination of Fantastic Four",
+      content:
+        "The MEAN Stack, comprising MongoDB, Express.js, Angular, and Node.js, is a powerful combination of technologies that work seamlessly together to build dynamic, high-performance web applications. Each component plays a crucial role in delivering a full-stack development experience: MongoDB offers a scalable, NoSQL database; Express.js simplifies server-side development; Angular provides a robust front-end framework; and Node.js powers the backend with its event-driven, non-blocking I/O model. Together, these four technologies form a robust, efficient, and scalable solution for modern web development, making the MEAN Stack an ideal choice for developers seeking to create innovative, end-to-end applications.",
+    },
+    expertTalkSection: {
+      content:
+        "Robotronix has established itself as a leading name in MEAN Stack development, delivering exceptional products to clients worldwide. Since its inception, Robotronix has specialized in offering tailored MEAN Stack solutions, producing innovative and industry-defining products that reflect our creative approach and forward-thinking development strategies.",
+    },
+    techStackSection: {
+      title: "Tech Stack Elements in MEAN",
+      content:
+        "The MEAN Stack is a popular and versatile full-stack JavaScript framework designed for building dynamic web applications. MEAN stands for MongoDB, Express JS, Node JS.",
+      cardData: [
+        {
+          heading: "MongoDB for storage",
+          content:
+            "MongoDB is an open-source NoSQL database that allows enterprises to be more agile and expandable. MongoDB has a flexible structure and can support a variety of formats of data.",
         },
-        infoSection:{
-            title:"The Combination of Fantastic Four",
-            content:"The MEAN Stack, comprising MongoDB, Express.js, Angular, and Node.js, is a powerful combination of technologies that work seamlessly together to build dynamic, high-performance web applications. Each component plays a crucial role in delivering a full-stack development experience: MongoDB offers a scalable, NoSQL database; Express.js simplifies server-side development; Angular provides a robust front-end framework; and Node.js powers the backend with its event-driven, non-blocking I/O model. Together, these four technologies form a robust, efficient, and scalable solution for modern web development, making the MEAN Stack an ideal choice for developers seeking to create innovative, end-to-end applications."
+        {
+          heading: "ExpressJs for web programming",
+          content:
+            "Express.JS is a lightweight but adaptable Node.JS web-based application framework that gives us a wide range of capabilities to create single-page web applications and multipage web and mobile apps. Node.JS facilitates routing.",
         },
-        expertTalkSection:{
-            content:"Robotronix has established itself as a leading name in MEAN Stack development, delivering exceptional products to clients worldwide. Since its inception, Robotronix has specialized in offering tailored MEAN Stack solutions, producing innovative and industry-defining products that reflect our creative approach and forward-thinking development strategies."
+        {
+          heading: "AngularJS for front-end development",
+          content:
+            "AngularJS is an open-source web application framework designed for dynamic web applications, which allows you to expand the capabilities of your HTML language application. This JavaScript-developed development framework is clear and easy to read and build.",
         },
-        techStackSection:{
-            title:"Tech Stack Elements in MEAN",
-            content:"The MEAN Stack is a popular and versatile full-stack JavaScript framework designed for building dynamic web applications. MEAN stands for MongoDB, Express JS, Node JS.",
-            cardData:[
-                {
-                  heading: "MongoDB for storage",
-                  content:
-                    "MongoDB is an open-source NoSQL database that allows enterprises to be more agile and expandable. MongoDB has a flexible structure and can support a variety of formats of data.",
-                },
-                {
-                  heading: "ExpressJs for web programming",
-                  content:
-                    "Express.JS is a lightweight but adaptable Node.JS web-based application framework that gives us a wide range of capabilities to create single-page web applications and multipage web and mobile apps. Node.JS facilitates routing.",
-                },
-                {
-                  heading: "AngularJS for front-end development",
-                  content:
-                    "AngularJS is an open-source web application framework designed for dynamic web applications, which allows you to expand the capabilities of your HTML language application. This JavaScript-developed development framework is clear and easy to read and build.",
-                },
-                {
-                  heading: "NodeJS for Web Server",
-                  content:
-                    "Node JS is an open-source, cross-platform used to create efficient and scalable server-side apps. Node.JS apps are developed with Google’s V8 JavaScript Engine, with an extensive JavaScript modules library.",
-                },
-                
-              ]
+        {
+          heading: "NodeJS for Web Server",
+          content:
+            "Node JS is an open-source, cross-platform used to create efficient and scalable server-side apps. Node.JS apps are developed with Google’s V8 JavaScript Engine, with an extensive JavaScript modules library.",
         },
-        hiringSection:{
-            title:"Bring on board expert MEAN stack developers",
-            content:"At Robotronix, our MEAN Stack specialists leverage the full potential of JavaScript technologies, utilizing cutting-edge tools and staying ahead of the latest market trends to deliver exceptional solutions. Our expert MEAN Stack team is equipped to provide tailored solutions for any industry, whether it's entertainment, eCommerce, banking, healthcare, or beyond, ensuring that each project aligns perfectly with our clients' unique needs and objectives.",
-            img:meanHiringImage
-        }
-    }
-  }
-  
+      ],
+    },
+    hiringSection: {
+      title: "Bring on board expert MEAN stack developers",
+      content:
+        "At Robotronix, our MEAN Stack specialists leverage the full potential of JavaScript technologies, utilizing cutting-edge tools and staying ahead of the latest market trends to deliver exceptional solutions. Our expert MEAN Stack team is equipped to provide tailored solutions for any industry, whether it's entertainment, eCommerce, banking, healthcare, or beyond, ensuring that each project aligns perfectly with our clients' unique needs and objectives.",
+      img: meanHiringImage,
+    },
+  },
+  reactDev: {
+    sectionFront: {
+      title: "ReactJS development company",
+      content:
+        "Create data-driven web apps and single page applications with ReactJS, a front-end JavaScript library, backed by Facebook.",
+      img: meanLogo,
+    },
+    infoSection: {
+      title: "Proven success with flexible ReactJS applications",
+      content:
+        "ReactJS is a JavaScript library that blends its speed for rendering websites making them extremely fluid and responsive to inputs from users. With React, developers are able to reuse components many times. Instead of writing the same code over and over, they can use React libraries to discover short codes that other developers have created, integrate them into their workflows, and then get ahead with rest of the coding tasks.",
+    },
+    expertTalkSection: {
+      content:
+        "React could be described as the “learn once and write anywhere” library since it is used for mobile and web application development, it follows the same design patterns that facilitate the process of transition. By using simple JavaScript and React, you’ll be able build a robust UI for native apps that are supported by the iOS as well as Android platforms.",
+    },
+    techStackSection: {
+      title: "The unmatched benefits of ReactJS development",
+      content:
+        "ReactJS is among the most well-known and widely used open-source platform-independent JavaScript UI frameworks. It offers ReactJS creators with a powerful and high-scalability framework to build user interfaces that are excellent for websites and applications.",
+      cardData: [
+        {
+          heading: "Components library",
+          content:
+            "ReactJS as a technology based on components that allows developers reuse and share different components to serve a variety of purposes.",
+        },
+        {
+          heading: "Declarative",
+          content:
+            "React is a simple way to build interactive UIs by the ability to quickly update and render the correct components whenever the data is changed.",
+        },
+        {
+          heading: "Virtual DOM",
+          content:
+            "If any changes are made in the application, the UI is re-rendered in the virtual DOM. The DOM updates only the changed section and everything remains same.",
+        },
+        {
+          heading: "No dependencies",
+          content:
+            "ReactJS is completely independent of other technologies. Developers are able to work without worrying about managing dependencies from outside ReactJS.",
+        },
+        {
+          heading: "SEO friendly",
+          content:
+            "As compared to other frameworks, ReactJS offers faster-rendering speed with reduced page loading time. With faster rendering speed, the bounce rate of websites and pages is also reduced.",
+        },
+        {
+          heading: "Responsive UI",
+          content:
+            "React renders responsive UI capabilities to create extensive user-interfaces that can fit multiple browsers, screen resolutions, and devices.",
+        },
+      ],
+    },
+    hiringSection: {
+      title: "Hire dedicated ReactJS developers",
+      content:
+        "At Robotronix, our React.js specialists harness the full potential of modern JavaScript technologies, utilizing the latest tools and staying ahead of market trends to deliver exceptional solutions. Our expert React.js team is equipped to provide tailored solutions for any industry—whether it's entertainment, eCommerce, banking, healthcare, or beyond—ensuring that each project aligns perfectly with our clients' unique needs and objectives.",
+      img: meanHiringImage,
+    },
+  },
+};
